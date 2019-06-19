0935dbf50b4b95761242fb398c7b5d1420ef499b
sapphire-al2o3 <t_telkun@hotmail.com>
2019-06-19 12:18:54 +0900

    整数から文字列への変換のテスト追加

--- Assets/StringTest/StringTest.cs
+++ Assets/StringTest/StringTest.cs
@@ -5,33 +5,33 @@ using UnityEngine.Profiling;
 
 public class StringTest : MonoBehaviour
 {
-	void Start()
-	{
-		string s0 = "aabbccddeeff";
-		string s1 = "aa,bb,ccddee";
+    void Start()
+    {
+        string s0 = "aabbccddeeff";
+        string s1 = "aa,bb,ccddee";
 
-		// 90byte
-		Profiler.BeginSample("split 1");
-		string[] array = s0.Split(',');
-		Profiler.EndSample();
+        // 90byte
+        Profiler.BeginSample("split 1");
+        string[] array = s0.Split(',');
+        Profiler.EndSample();
 
-		// 48byte
-		Profiler.BeginSample("no split");
-		if (s0.IndexOf(',') >= 0)
-		{
-			
-		}
-		else
-		{
-			array = new string[] { s0 };
-		}
-		Profiler.EndSample();
+        // 48byte
+        Profiler.BeginSample("no split");
+        if (s0.IndexOf(',') >= 0)
+        {
 
-		// 204byte
-		Profiler.BeginSample("split 3");
-		array = s1.Split(',');
-		Profiler.EndSample();
-        
+        }
+        else
+        {
+            array = new string[] { s0 };
+        }
+        Profiler.EndSample();
+
+        // 204byte
+        Profiler.BeginSample("split 3");
+        array = s1.Split(',');
+        Profiler.EndSample();
+
         string[] num = { "0", "1", "2", "3", "4", "5" };
 
         Profiler.BeginSample("concat3");
@@ -42,5 +42,29 @@ public class StringTest : MonoBehaviour
         s = num[0] + num[1] + num[2] + num[3];
         s += num[4];
         Profiler.EndSample();
+
+        // 32byte
+        {
+            Profiler.BeginSample("int -> string (ToString)");
+            int i0 = 100;
+            s = i0.ToString();
+            Profiler.EndSample();
+        }
+
+        // 84byte
+        {
+            Profiler.BeginSample("int -> string (Format)");
+            int i0 = 100;
+            s = string.Format("{0}", i0);
+            Profiler.EndSample();
+        }
+
+        // 84byte
+        {
+            Profiler.BeginSample("int -> string ($)");
+            int i0 = 100;
+            s = $"{i0}";
+            Profiler.EndSample();
+        }
     }
 }
