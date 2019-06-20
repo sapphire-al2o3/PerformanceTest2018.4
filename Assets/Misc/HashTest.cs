692eb84acd218810f0bb310db1bb7ad98b039bd9
sapphire-al2o3 <t_telkun@hotmail.com>
2019-06-20 17:52:41 +0900

    MD5のインスタンスを生成する際のテストを追加

---
+++ Assets/Misc/HashTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Security.Cryptography;
+using UnityEngine.Profiling;
+
+public class HashTest : MonoBehaviour
+{
+    void Start()
+    {
+        // 320B
+        {
+            Profiler.BeginSample("MD5CryptoServiceProvider");
+            var md5 = new MD5CryptoServiceProvider();
+            Profiler.EndSample();
+
+            Debug.Log(md5.GetType());
+        }
+
+        // 1.5KB
+        {
+            Profiler.BeginSample("MD5.Create");
+            var md5 = MD5.Create();
+            Profiler.EndSample();
+
+            Debug.Log(md5.GetType());
+        }
+    }
+}
