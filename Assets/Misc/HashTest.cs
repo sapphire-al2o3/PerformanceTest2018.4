fb7d752a3fdc117627757c31e187b74ac89a34b7
sapphire-al2o3 <t_telkun@hotmail.com>
2019-06-23 22:10:56 +0900

    ハッシュ計算のメモリ計測追加

--- Assets/Misc/HashTest.cs
+++ Assets/Misc/HashTest.cs
@@ -8,6 +8,30 @@ public class HashTest : MonoBehaviour
 {
     void Start()
     {
+        byte[] data = { 0, 1, 2, 3, 4, 5, 6, 7 };
+
+        // 192B
+        {
+            using (var md5 = new MD5CryptoServiceProvider())
+            {
+                Profiler.BeginSample("ComputeHash");
+                var hash = md5.ComputeHash(data);
+                Profiler.EndSample();
+            }
+        }
+
+        // 228B
+        {
+            using (var md5 = new MD5CryptoServiceProvider())
+            {
+                Profiler.BeginSample("TransformBlock");
+                md5.TransformBlock(data, 4, 4, null, 0);
+                md5.TransformFinalBlock(data, 4, 4);
+                var hash = md5.Hash;
+                Profiler.EndSample();
+            }
+        }
+
         // 320B
         {
             Profiler.BeginSample("MD5CryptoServiceProvider");
