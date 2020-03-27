514c1ad4af4ea698aa90f7a2a1fd2d38294076b9
sapphire-al2o3 <t_telkun@hotmail.com>
2020-03-27 15:17:23 +0900

    BinaryWriterの計測追加

---
+++ Assets/Misc/StreamTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Profiling;
+using System.IO;
+
+public class StreamTest : MonoBehaviour
+{
+    void Start()
+    {
+        using (var ms = new MemoryStream(1024))
+        using (var bw = new BinaryWriter(ms))
+        {
+            // 36byte
+            using (new ProfilerScope("float"))
+            {
+                float f = 1.0f;
+                bw.Write(f);
+            }
+
+            // 0byte
+            using (new ProfilerScope("int"))
+            {
+                int i = 1;
+                bw.Write(i);
+            }
+        }
+    }
+}
