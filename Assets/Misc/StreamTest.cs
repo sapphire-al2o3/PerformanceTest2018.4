7b36a315ab0ef57d89e45a3b4862a3535fcb87f6
sapphire-al2o3 <t_telkun@hotmail.com>
2020-03-28 23:09:18 +0900

    BinaryWriter.Writeでfloatをintにキャストする

--- Assets/Misc/StreamTest.cs
+++ Assets/Misc/StreamTest.cs
@@ -6,6 +6,12 @@ using System.IO;
 
 public class StreamTest : MonoBehaviour
 {
+    unsafe void UnsafeWrite(BinaryWriter bw, float value)
+    {
+        uint temp = *(uint*)&value;
+        bw.Write(temp);
+    }
+
     void Start()
     {
         using (var ms = new MemoryStream(1024))
@@ -17,12 +23,19 @@ public class StreamTest : MonoBehaviour
                 float f = 1.0f;
                 bw.Write(f);
             }
-
+            
             // 0byte
             using (new ProfilerScope("int"))
             {
                 int i = 1;
                 bw.Write(i);
+            }
+            
+            // 0byte
+            using (new ProfilerScope("float unsafe"))
+            {
+                float f = 1.0f;
+                UnsafeWrite(bw, f);
             }
         }
     }
