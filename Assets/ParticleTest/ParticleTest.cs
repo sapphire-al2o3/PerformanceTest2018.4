f5ef598fd0b00baa8564e50cd6ea81aba7ff0058
sapphire-al2o3 <t_telkun@hotmail.com>
2020-04-11 17:55:36 +0900

    warning修正

--- Assets/ParticleTest/ParticleTest.cs
+++ Assets/ParticleTest/ParticleTest.cs
@@ -2,18 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ParticleTest : MonoBehaviour {
+public class ParticleTest : MonoBehaviour
+{
+    [SerializeField]
+    GameObject ps = null;
 
-	[SerializeField]
-	GameObject ps;
-
-	void Start()
-	{
-		// trail使っていなくてもアクセスするとDefault-Materialがつく
-		var pr = ps.GetComponent<ParticleSystemRenderer>();
-		foreach (var m in pr.materials)
-		{
-			Debug.Log(m.name);
-		}
-	}
+    void Start()
+    {
+        // trail使っていなくてもアクセスするとDefault-Materialがつく
+        var pr = ps.GetComponent<ParticleSystemRenderer>();
+        foreach (var m in pr.materials)
+        {
+            Debug.Log(m.name);
+        }
+    }
 }
