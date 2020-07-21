221949ab0a7c81465d7463a1d8a90bf7f8ff5cde
sapphire-al2o3 <t_telkun@hotmail.com>
2020-07-21 23:57:00 +0900

    スコープを付ける

--- Assets/StringTest/StringTest.cs
+++ Assets/StringTest/StringTest.cs
@@ -16,40 +16,50 @@ public class StringTest : MonoBehaviour
         Profiler.EndSample();
 
         // 48byte
-        Profiler.BeginSample("no split");
-        if (s0.IndexOf(',') >= 0)
         {
+            Profiler.BeginSample("no split");
+            if (s0.IndexOf(',') >= 0)
+            {
 
+            }
+            else
+            {
+                array = new string[] { s0 };
+            }
+            Profiler.EndSample();
         }
-        else
-        {
-            array = new string[] { s0 };
-        }
-        Profiler.EndSample();
 
         // 204byte
-        Profiler.BeginSample("split 3");
-        array = s1.Split(',');
-        Profiler.EndSample();
+        {
+            Profiler.BeginSample("split 3");
+            array = s1.Split(',');
+            Profiler.EndSample();
+        }
 
         string[] num = { "0", "1", "2", "3", "4", "5" };
 
-        Profiler.BeginSample("concat3");
-        string s = num[0] + num[1] + num[2];
-        Profiler.EndSample();
+        {
+            Profiler.BeginSample("concat3");
+            string s = num[0] + num[1] + num[2];
+            Profiler.EndSample();
+        }
 
         // GC.Alloc x1 34B
-        Profiler.BeginSample("concat +");
-        s = num[0] + num[1] + num[2] + num[3];
-        Profiler.EndSample();
+        {
+            Profiler.BeginSample("concat +");
+            string s = num[0] + num[1] + num[2] + num[3];
+            Profiler.EndSample();
+        }
 
         // GC.Alloc x3 96B
-        Profiler.BeginSample("concat +=");
-        s = num[0];
-        s += num[1];
-        s += num[2];
-        s += num[3];
-        Profiler.EndSample();
+        {
+            Profiler.BeginSample("concat +=");
+            string s = num[0];
+            s += num[1];
+            s += num[2];
+            s += num[3];
+            Profiler.EndSample();
+        }
 
         // 150Byte
         {
@@ -59,7 +69,7 @@ public class StringTest : MonoBehaviour
             {
                 sb.Append(num[i]);
             }
-            s = sb.ToString();
+            string s = sb.ToString();
             Profiler.EndSample();
         }
 
@@ -88,7 +98,7 @@ public class StringTest : MonoBehaviour
         {
             Profiler.BeginSample("int -> string (ToString)");
             int i0 = 100;
-            s = i0.ToString();
+            string s = i0.ToString();
             Profiler.EndSample();
         }
 
@@ -96,7 +106,7 @@ public class StringTest : MonoBehaviour
         {
             Profiler.BeginSample("int -> string (Format)");
             int i0 = 100;
-            s = string.Format("{0}", i0);
+            string s = string.Format("{0}", i0);
             Profiler.EndSample();
         }
 
@@ -104,28 +114,28 @@ public class StringTest : MonoBehaviour
         {
             Profiler.BeginSample("int -> string ($)");
             int i0 = 100;
-            s = $"{i0}";
+            string s = $"{i0}";
             Profiler.EndSample();
         }
 
         // 28byte
         {
             Profiler.BeginSample("last char (SubString)");
-            s = s0.Substring(s0.Length - 1);
+            string s = s0.Substring(s0.Length - 1);
             Profiler.EndSample();
         }
 
         // 28byte
         {
             Profiler.BeginSample("last char (ToString)");
-            s = s0[s0.Length - 1].ToString();
+            string s = s0[s0.Length - 1].ToString();
             Profiler.EndSample();
         }
 
         // 48byte
         {
             Profiler.BeginSample("join");
-            s = string.Join(",", num);
+            string s = string.Join(",", num);
             Profiler.EndSample();
         }
 
@@ -138,26 +148,26 @@ public class StringTest : MonoBehaviour
                 sb.Append(num[0]);
                 sb.Append(",");
             }
-            s = sb.ToString();
+            string s = sb.ToString();
             Profiler.EndSample();
         }
 
         {
             Profiler.BeginSample("concat null");
-            s = s0 + null;
+            string s = s0 + null;
             Profiler.EndSample();
         }
 
         {
             Profiler.BeginSample("concat empty");
-            s = s0 + "";
+            string s = s0 + "";
             Profiler.EndSample();
         }
 
         // 0byte
         {
             Profiler.BeginSample("String.ToString");
-            s = s0.ToString();
+            string s = s0.ToString();
             Profiler.EndSample();
         }
 
