033e81030ba553e878a09c5f20ee6f2294be31e1
sapphire-al2o3 <t_telkun@hotmail.com>
2020-10-03 04:32:41 +0900

    ファイルの存在確認関数の計測をべつシーンにわける

--- Assets/Misc/FileTest.cs
+++ Assets/Misc/FileTest.cs
@@ -12,26 +12,47 @@ public class FileTest : MonoBehaviour
 	[SerializeField]
 	string dirPath = null;
 
-    void Start()
+    private void Start()
     {
-        filePath = Application.dataPath + filePath;
-        dirPath = Application.dataPath + dirPath;
+#if UNITY_EDITOR
+        Run();
+#endif
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Run();
+        }
+    }
+
+
+    void Run()
+    {
+        string file = Application.dataPath + filePath;
+        string dir = Application.dataPath + dirPath;
 
         Debug.Log(Application.dataPath);
 
         // 0B
+        // IL2CPP 0.5KB
+        // IL2CPP 2回目以降 0B
         {
             Profiler.BeginSample("Exists File");
-            bool exists = File.Exists(filePath);
+            bool exists = File.Exists(file);
             Profiler.EndSample();
 
             Debug.Log(exists);
         }
 
         // 0.9KB
+        // IL2CPP 1.5KB
+        // IL2CPP 2回目以降 0.8KB
+        // パスの長さに依存する
         {
             Profiler.BeginSample("Exists Directory");
-            bool exists = Directory.Exists(dirPath);
+            bool exists = Directory.Exists(dir);
             Profiler.EndSample();
 
             Debug.Log(exists);
