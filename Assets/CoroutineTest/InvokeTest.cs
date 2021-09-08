fd6496b43a1a00acec4c084fc9ba0dbee698052e
sapphire-al2o3 <t_telkun@hotmail.com>
2021-09-08 21:45:04 +0900

    Invokeの計測追加

---
+++ Assets/CoroutineTest/InvokeTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Profiling;
+
+public class InvokeTest : MonoBehaviour
+{
+	void Func()
+	{
+
+	}
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		// InvokeMethodOrCoroutineCheckedでGC Allocが発生する
+		// 72byte
+		{
+			Profiler.BeginSample("Invoke");
+			Invoke("Func", 0.0f);
+			Profiler.EndSample();
+		}
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
