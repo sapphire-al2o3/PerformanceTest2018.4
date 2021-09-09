f11f9f5625137d999eccccf11d2a5a502ad9fe87
sapphire-al2o3 <t_telkun@hotmail.com>
2021-09-09 18:34:25 +0900

    インデント修正

--- Assets/CoroutineTest/InvokeTest.cs
+++ Assets/CoroutineTest/InvokeTest.cs
@@ -5,26 +5,19 @@ using UnityEngine.Profiling;
 
 public class InvokeTest : MonoBehaviour
 {
-	void Func()
-	{
-
-	}
-
-	// Start is called before the first frame update
-	void Start()
-	{
-		// InvokeMethodOrCoroutineCheckedでGC Allocが発生する
-		// 72byte
-		{
-			Profiler.BeginSample("Invoke");
-			Invoke("Func", 0.0f);
-			Profiler.EndSample();
-		}
-	}
-
-    // Update is called once per frame
-    void Update()
+    void Func()
     {
-        
+
+    }
+
+    void Start()
+    {
+        // InvokeMethodOrCoroutineCheckedでGC Allocが発生する
+        // 72byte
+        {
+            Profiler.BeginSample("Invoke");
+            Invoke("Func", 0.0f);
+            Profiler.EndSample();
+        }
     }
 }
